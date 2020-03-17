d7c5fc392eb191dc5c096a429eda941f49ff6517
sewvandipw.98@gmail.com <sewvandi.promodya@gmail.com>
2020-03-17 17:07:08 +0530

    Add Signin, Signup buttons

--- app/src/main/java/com/example/eatapp/MainActivity.java
+++ app/src/main/java/com/example/eatapp/MainActivity.java
@@ -3,12 +3,23 @@ package com.example.eatapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btnSignUp, btnSignIn;
+    TextView texteatapp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnSignIn = (Button)findViewById(R.id.buttonsignin);
+        btnSignUp = (Button)findViewById(R.id.buttonsignup);
+
+        texteatapp = (TextView)findViewById(R.id.texteatapp);
+
     }
 }
